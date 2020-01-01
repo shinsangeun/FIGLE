0642c105202a14e66d4f7d5c3c155325cd911ed3
shinsangeun <s0813se@naver.com>
2020-01-01 17:12:13 +0900

    react 템플릿 추가

---
+++ src/components/Footers/AdminFooter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// reactstrap components
+import { Container, Row, Col, Nav, NavItem, NavLink } from "reactstrap";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer">
+        <Row className="align-items-center justify-content-xl-between">
+        </Row>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
