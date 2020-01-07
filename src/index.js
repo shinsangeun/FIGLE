21aadd8dba4417058c8bd69b66b4149c7bd01001
이준서 <l035833@naver.com>
2020-01-07 17:33:16 +0900

    주석 추가

--- src/index.js
+++ src/index.js
@@ -9,9 +9,14 @@ import "assets/scss/argon-dashboard-react.scss";
 import AdminLayout from "layouts/Admin.jsx";
 import AuthLayout from "layouts/Auth.jsx";
 
+/*
+Switch : 이 컴포넌트에 추가된 첫 번째 Route만 보여주고 나머지는 보여주지 않음.(주의 : 먼저 비교할 Route를 맨 위에 둬야 함.)
+         path 중 '/'를 switch 맨 위에 두지 말 것.
+BrowserRouter : 브라우저와 웹 사이에 존재하는 라우터 임포팅하는 것으로 브라우저들이 동작하게 만듦
+*/
 ReactDOM.render(
   <BrowserRouter>
-    <Switch>
+    <Switch> 
       <Route path="/admin" render={props => <AdminLayout {...props} />} />
       <Route path="/auth" render={props => <AuthLayout {...props} />} />
       <Redirect from="/" to="/admin/index" />
@@ -19,3 +24,4 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+
--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -10,15 +10,21 @@ import Sidebar from "components/Sidebar/Sidebar.jsx";
 import routes from "routes.js";
 
 class Admin extends React.Component {
-  componentDidUpdate(e) {
+
+  // Props 또는 State가 변경이 되면 갱신 발생
+  componentDidUpdate(e) 
+  { 
     document.documentElement.scrollTop = 0;
     document.scrollingElement.scrollTop = 0;
     this.refs.mainContent.scrollTop = 0;
   }
-  getRoutes = routes => {
-    return routes.map((prop, key) => {
-      if (prop.layout === "/admin") {
+
+  //getRoutes 함수 선언
+  getRoutes = routes => { // routes라는 파라미터 변수 선언
+    return routes.map((prop, key) => { // map 함수를 통하여 routes로 값이 들어올 때마다 반복하여 반환
+      if (prop.layout === "/admin") { // 주소창의 경로가 /admin일 경우
         return (
+          // 라우팅, 아래와 같은 형식으로 경로 및 컴포넌트 반환
           <Route
             path={prop.layout + prop.path}
             component={prop.component}
@@ -30,7 +36,10 @@ class Admin extends React.Component {
       }
     });
   };
+
+  //getBrandText 함수 선언
   getBrandText = path => {
+    // routes.js 파일에서 반환된 값의 길이 만큼 반복
     for (let i = 0; i < routes.length; i++) {
       if (
         this.props.location.pathname.indexOf(
@@ -42,6 +51,8 @@ class Admin extends React.Component {
     }
     return "Brand";
   };
+
+  // 페이지 렌더링
   render() {
     return (
       <>
