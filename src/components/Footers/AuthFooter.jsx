cde9b1e387f18117b1e8dd9077084d9f1d7d86a6
shinsangeun <s0813se@naver.com>
2020-01-11 20:22:56 +0900

    1. /auth/Search 페이지 추가(초기 화면 페이지) -사이드 메뉴에 링크 추가 필요 2. footer: figle로 수정

--- src/components/Footers/AuthFooter.jsx
+++ src/components/Footers/AuthFooter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 // reactstrap components
-import { NavItem, NavLink, Nav, Container, Row, Col } from "reactstrap";
+import {NavItem, NavLink, Nav, Container, Row, Col, Button} from "reactstrap";
 
 class Login extends React.Component {
   render() {
@@ -12,50 +12,25 @@ class Login extends React.Component {
             <Row className="align-items-center justify-content-xl-between">
               <Col xl="6">
                 <div className="copyright text-center text-xl-left text-muted">
-                  © 2018{" "}
-                  <a
-                    className="font-weight-bold ml-1"
-                    href="https://www.creative-tim.com?ref=adr-auth-footer"
-                    target="_blank"
-                  >
-                    Creative Tim
-                  </a>
+                  © 2020{" "}
+                    FIGLE
                 </div>
               </Col>
               <Col xl="6">
                 <Nav className="nav-footer justify-content-center justify-content-xl-end">
-                  <NavItem>
-                    <NavLink
-                      href="https://www.creative-tim.com?ref=adr-auth-footer"
-                      target="_blank"
-                    >
-                      Creative Tim
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      href="https://www.creative-tim.com/presentation?ref=adr-auth-footer"
-                      target="_blank"
-                    >
-                      About Us
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      href="http://blog.creative-tim.com?ref=adr-auth-footer"
-                      target="_blank"
-                    >
-                      Blog
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink
-                      href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md?ref=adr-auth-footer"
-                      target="_blank"
-                    >
-                      MIT License
-                    </NavLink>
-                  </NavItem>
+                  <Button
+                      className="btn-neutral btn-icon"
+                      color="default"
+                      href="https://github.com/gksthf2271/FIGLE/"
+                  >
+                  <span className="btn-inner--icon">
+                    <img
+                        alt="..."
+                        src={require("assets/img/icons/common/github.svg")}
+                    />
+                  </span>
+                    <span className="btn-inner--text">Github</span>
+                  </Button>
                 </Nav>
               </Col>
             </Row>
