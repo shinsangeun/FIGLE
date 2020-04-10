726c8c52869af049c90b58cf5ba39eeb1cf58f2f
shinsangeun <s0813se@naver.com>
2020-04-10 16:21:19 +0900

    /auth/search 아이디 검색 기능 추가

--- src/components/Navbars/AdminNavbar.jsx
+++ src/components/Navbars/AdminNavbar.jsx
@@ -20,33 +20,26 @@ import {
 import axios from "axios";
 
 class AdminNavbar extends React.Component {
-  constructor()
-  {
-    super()
-
+  constructor(props) {
+    super(props);
     this.state ={
-      //닉네임
-      nickname:'',
-      //아이디 시리얼 넘버
-      accessId:'',
-      //레벨
-      level:0,
+      nickname:'',    //닉네임
+      accessId:'',    //아이디 시리얼 넘버
+      level:0,        //레벨
     };
-
-    // 이벤트 호출이 텍스트에 입력하고 검색 버튼을 눌렀을 때 나오도록 바꾸어야 함
-    this.getUserId('아무거나다잘함')
   }
 
-  getUserId = user_name => {
-
-    var req_message = 'https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=' + user_name
+  async getUserId(nickname){
+    let req_message = 'https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=' + nickname;
     try{
-
-      return axios.get(req_message, {
+      return await axios.get(req_message, {
         // 헤더 값 : 권한 시리얼 정보
         headers : { Authorization : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTIyNDc2MTUyOSIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTQ4MSIsIlgtQXBwLVJhdGUtTGltaXQiOiIyMDAwMDoxMCIsIm5iZiI6MTU3NzAwODc3MywiZXhwIjoxNjQwMDgwNzczLCJpYXQiOjE1NzcwMDg3NzN9.Pv1OIow11dye_uv69wnVleR93fa4fDrmup1oTXVuUuo'}
-
-      }).then(response => this.setState({nickname : response.data.nickname, accessId : response.data.accessId, level : response.data.level}))
+      }).then(response => this.setState({
+        nickname : response.data.nickname,
+        accessId : response.data.accessId,
+        level : response.data.level
+      }))
     }
     catch (error)
     {
--- src/layouts/Auth.jsx
+++ src/layouts/Auth.jsx
@@ -1,21 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 // reactstrap components
-import { Container, Row, Col } from "reactstrap";
+import {Container, Row, Col, Form, InputGroup, Input, InputGroupAddon, InputGroupText, Collapse, Button} from "reactstrap";
 
 // core components
 import AuthNavbar from "components/Navbars/AuthNavbar.jsx";
 import AuthFooter from "components/Footers/AuthFooter.jsx";
-
 import routes from "routes.js";
+import axios from "axios";
 
 class Auth extends React.Component {
-  componentDidMount() {
-    document.body.classList.add("bg-default");
-  }
-  componentWillUnmount() {
-    document.body.classList.remove("bg-default");
-  }
   getRoutes = routes => {
     return routes.map((prop, key) => {
       if (prop.layout === "/auth") {
@@ -31,6 +25,43 @@ class Auth extends React.Component {
       }
     });
   };
+
+  constructor(props) {
+    super(props);
+    this.state ={
+      nickname:'',  //닉네임
+      accessId:'',  //아이디 시리얼 넘버
+      level:0,      //레벨
+    };
+  }
+
+  handleChange = (e) => {
+    // 페이지 리로딩 방지
+    e.preventDefault();
+    this.setState({
+      nickname: e.target.value
+    });
+    this.getUserId(e.target.value);
+  };
+
+  async getUserId(nickname){
+    let req_message = 'https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=' + nickname;
+    try{
+      return await axios.get(req_message, {
+        // 헤더 값 : 권한 시리얼 정보
+        headers : { Authorization : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTIyNDc2MTUyOSIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTQ4MSIsIlgtQXBwLVJhdGUtTGltaXQiOiIyMDAwMDoxMCIsIm5iZiI6MTU3NzAwODc3MywiZXhwIjoxNjQwMDgwNzczLCJpYXQiOjE1NzcwMDg3NzN9.Pv1OIow11dye_uv69wnVleR93fa4fDrmup1oTXVuUuo'}
+      }).then(response => this.setState({
+        nickname : response.data.nickname,
+        accessId : response.data.accessId,
+        level : response.data.level
+      }))
+    }
+    catch (error)
+    {
+      console.error(error);
+    }
+  };
+
   render() {
     return (
       <>
@@ -49,6 +80,25 @@ class Auth extends React.Component {
                 </Row>
               </div>
             </Container>
+
+              <form className="navbar-search navbar-search-dark">
+                  <div className="mb-0 form-group">
+                      <div className="input-group-alternative input-group">
+                          <div className="input-group-prepend"><span className="input-group-text">
+                              <i className="fas fa-search" ></i></span></div>
+                          <input placeholder="Search" type="text" className="form-control"
+                                 name="nickname"
+                                 value={this.state.nickname}
+                                 onChange={this.handleChange}
+                          ></input>
+                        <Button type="submit" href='/admin/tables/'>검색</Button>
+                      </div>
+                  </div>
+              </form>
+              {/*<li>닉네임: {this.state.nickname}</li>
+              <li>레벨: {this.state.level}</li>
+              <li>access ID: {this.state.accessId} </li>*/}
+
             <div className="separator separator-bottom separator-skew zindex-100">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
