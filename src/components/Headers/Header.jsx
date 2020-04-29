abe8e26146826781341e66f3fd07c176be831e1e
shinsangeun <s0813se@naver.com>
2020-04-29 23:56:30 +0900

    경기보기 detail 데이터 추가

--- src/components/Headers/Header.jsx
+++ src/components/Headers/Header.jsx
@@ -1,10 +1,81 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom';
 
 // reactstrap components
 import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
+import axios from "axios";
 
 class Header extends React.Component {
+  state = {
+    isLoading: false,
+    accessId: '',
+    matchResult: []
+  };
+
+  // 매치 상세 기록 조회
+  _getMatchIdDetail = () => {
+    let MatchIdList = this.state.matchIdList;
+    let matchResultArray = [];
+
+    for (let i = 0; i < MatchIdList.length; i++) {
+      if(MatchIdList.length === 0){
+        return;
+      }else{
+        let getMatchIdDetail = 'https://api.nexon.co.kr/fifaonline4/v1.0/matches/' + MatchIdList[i];
+        try{
+          axios.get(getMatchIdDetail, {
+            // 헤더 값 : 권한 시리얼 정보
+            headers: {Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTIyNDc2MTUyOSIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTQ4MSIsIlgtQXBwLVJhdGUtTGltaXQiOiIyMDAwMDoxMCIsIm5iZiI6MTU3NzAwODc3MywiZXhwIjoxNjQwMDgwNzczLCJpYXQiOjE1NzcwMDg3NzN9.Pv1OIow11dye_uv69wnVleR93fa4fDrmup1oTXVuUuo'}
+          }).then(response => {
+            matchResultArray.push(response.data);
+            this.setState({
+              matchResult: matchResultArray,
+              isLoading: false
+            })
+          })
+        }catch(error){
+          console.error(error);
+        }
+      }
+    }
+  };
+
+  // 경기 결과 버튼 색깔
+  handleButton = (matchResult) => {
+    if(matchResult === "승"){
+      let bg_color = "success";
+      return bg_color;
+    }else if(matchResult === "패"){
+      let bg_color = "warning";
+      return bg_color;
+    }else if(matchResult === "무"){
+      let bg_color = "secondary";
+      return bg_color;
+    }
+  };
+
+  componentDidMount= () => {
+   // const search = this.props; //.location.search;
+   /* console.log("search::", search);
+
+    const params = new URLSearchParams(search);*/
+  /*  const accessId = params.get("accessId");
+    console.log("accessId:: ", accessId);
+
+    this.setState({
+      accessId: accessId
+    });*/
+  };
+
   render() {
+    const search = this.props;
+
+    const {accessId, matchResult, isLoading} = this.props;
+
+    if(!isLoading){
+      console.log("test:", matchResult)
+    }
+
     return (
       <>
         <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
--- src/views/examples/Profile.jsx
+++ src/views/examples/Profile.jsx
@@ -3,22 +3,21 @@ import axios from "axios";
 
 // reactstrap components
 import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  FormGroup,
-  Form,
-  Input,
-  Container,
-  Row,
-  Col
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    FormGroup,
+    Form,
+    Input,
+    Container,
+    Row,
+    Col, Table
 } from "reactstrap";
 // core components
 import UserHeader from "components/Headers/UserHeader.jsx";
 
 class Profile extends React.Component {
-  
   constructor(props) {
     super(props);
       this.state ={
@@ -31,7 +30,7 @@ class Profile extends React.Component {
   }
 
   componentDidMount = async () => {
-    await this.getMatchIdDatail();
+      await this.getMatchIdDatail();
   };
 
   getMatchIdDatail(){
@@ -47,6 +46,7 @@ class Profile extends React.Component {
         headers: {Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTIyNDc2MTUyOSIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTQ4MSIsIlgtQXBwLVJhdGUtTGltaXQiOiIyMDAwMDoxMCIsIm5iZiI6MTU3NzAwODc3MywiZXhwIjoxNjQwMDgwNzczLCJpYXQiOjE1NzcwMDg3NzN9.Pv1OIow11dye_uv69wnVleR93fa4fDrmup1oTXVuUuo'}
       }).then(response => {
         matchResultArray.push(response.data);
+        console.log("matchResult:" , matchResultArray[0].matchInfo[0].matchDetail.possession)
         this.setState({
           matchResult: matchResultArray,
           isLoading: false
@@ -60,9 +60,10 @@ class Profile extends React.Component {
   render() {
     const {matchResult, isLoading} = this.state;
 
-    if(!isLoading) {
-      console.log("res::", matchResult[0].matchInfo[0].nickname);
-      console.log(this.state.matchResult[0].matchInfo[0].nickname);
+    if(!isLoading){
+        console.log("res:", matchResult[0].matchInfo[0].nickname);
+        console.log(this.state.matchResult[0].matchInfo[0].nickname);
+        console.log("test:", this.state.matchResult[0].matchInfo[0].matchDetail.possession)
     }
 
     return (
@@ -86,24 +87,29 @@ class Profile extends React.Component {
                     </div>
                   </Col>
                 </Row>
+                  {isLoading ? (
+                      <div className="loader">
+                          <span className="loader__text">Loading...</span>
+                      </div>
+                  ) : (
                 <CardBody className="pt-0 pt-md-4">
                   <Row>
                     <div className="col">
                       <div className="card-profile-stats d-flex justify-content-center mt-md-5">
                         <div>
-                          <span className="heading">{this.state.level}</span>
+                          <span className="heading">{this.state.matchResult[0].matchInfo[0].matchDetail.possession}</span>
                           <span className="description">점유율</span>
                         </div>
                         <div>
-                          <span className="heading">10</span>
+                          <span className="heading">{this.state.matchResult[0].matchInfo[0].matchDetail.foul}</span>
                           <span className="description">파울</span>
                         </div>
                         <div>
-                          <span className="heading">89</span>
+                          <span className="heading">{this.state.matchResult[0].matchInfo[0].matchDetail.cornerKick}</span>
                           <span className="description">코너킥</span>
                         </div>
                           <div>
-                              <span className="heading">89</span>
+                              <span className="heading">{this.state.matchResult[0].matchInfo[0].matchDetail.yellowCards}</span>
                               <span className="description">카드</span>
                           </div>
                           <div>
@@ -122,11 +128,10 @@ class Profile extends React.Component {
                     </div>
                   </Row>
                   <div className="text-center">
-                    {/*{matchResult.map((index, matchId) => (*/}
-                    <h3>닉네임 </h3>
-                      {/*  ))}*/}
+                    <h3>{this.state.matchResult[0].matchInfo[0].nickname}</h3>
                   </div>
                 </CardBody>
+                  )}
               </Card>
             </Col>
               <Col className="order-xl-2 mb-5 mb-xl-0" xl="6">
@@ -144,24 +149,29 @@ class Profile extends React.Component {
                               </div>
                           </Col>
                       </Row>
+                      {isLoading ? (
+                          <div className="loader">
+                              <span className="loader__text">Loading...</span>
+                          </div>
+                      ) : (
                       <CardBody className="pt-0 pt-md-4">
                           <Row>
                               <div className="col">
                                   <div className="card-profile-stats d-flex justify-content-center mt-md-5">
                                       <div>
-                                          <span className="heading">{this.state.level}</span>
+                                          <span className="heading">{this.state.matchResult[0].matchInfo[1].matchDetail.possession}</span>
                                           <span className="description">점유율</span>
                                       </div>
                                       <div>
-                                          <span className="heading">10</span>
+                                          <span className="heading">{this.state.matchResult[0].matchInfo[1].matchDetail.foul}</span>
                                           <span className="description">파울</span>
                                       </div>
                                       <div>
-                                          <span className="heading">89</span>
+                                          <span className="heading">{this.state.matchResult[0].matchInfo[1].matchDetail.cornerKick}</span>
                                           <span className="description">코너킥</span>
                                       </div>
                                       <div>
-                                          <span className="heading">89</span>
+                                          <span className="heading">{this.state.matchResult[0].matchInfo[1].matchDetail.yellowCards}</span>
                                           <span className="description">카드</span>
                                       </div>
                                       <div>
@@ -180,11 +190,10 @@ class Profile extends React.Component {
                               </div>
                           </Row>
                           <div className="text-center">
-                              {/*{matchResult.map((index, matchId) => (*/}
-                              <h3>닉네임 </h3>
-                              {/*  ))}*/}
+                              <h3>{this.state.matchResult[0].matchInfo[1].nickname}</h3>
                           </div>
                       </CardBody>
+                      )}
                   </Card>
               </Col>
           </Row>
