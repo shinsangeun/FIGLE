28a35685247706b6bdcfc89684090b128e12ccc0
shinsangeun <s0813se@naver.com>
2020-10-03 23:54:52 +0900

    Header 수정

--- src/components/Headers/Header.jsx
+++ src/components/Headers/Header.jsx
@@ -4,24 +4,100 @@ import { Route, Link } from 'react-router-dom';
 // reactstrap components
 import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
 import axios from "axios";
+import { HashRouter } from 'react-router-dom'
 
 class Header extends React.Component {
-  state = {
-    isLoading: false,
-    accessId: ''
+  constructor(props) {
+    super(props);
+    this.state ={
+      accessId:'',
+      isLoading: true,
+      matchResult:[]
+    };
+  }
+
+  componentDidMount= async () => {
+    await this._getMatchIdList();
+    await this._getMatchIdDetail();
   };
 
-  componentDidMount= () => {
+  // 유저 닉네임으로 유저 정보 조회
+  _getMatchIdList = () => {
+    ReactDOM.render(
+        <HashRouter>
+          <Route path='' component={App}/>
+        </HashRouter>,
+        document.getElementById('search'));
 
+    const search = this.props.location.search;
+    console.log("search:", search);
+    const params = new URLSearchParams(search);
+    console.log("params:", params);
+    const accessId = params.get("accessId");
+    const matchtype = 50;                              //50: 공식 경기, 52: 감독 모드
+    const offset = 0;                                  // 리스트에서 가져올 시작 위치
+    const limit = 100;                                 // 리스트에서 가져올 갯수
+    console.log("accessId:", accessId);
+
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    let getMatchIdList = 'https://api.nexon.co.kr/fifaonline4/v1.0/users/' + accessId + '/matches?matchtype=' + matchtype + '&offset=' + offset +'&limit=' + limit;
+    try{
+      return axios.get(proxyurl + getMatchIdList, {
+        // 헤더 값 : 권한 시리얼 정보
+        headers : { Authorization : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTIyNDc2MTUyOSIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTQ4MSIsIlgtQXBwLVJhdGUtTGltaXQiOiIyMDAwMDoxMCIsIm5iZiI6MTU3NzAwODc3MywiZXhwIjoxNjQwMDgwNzczLCJpYXQiOjE1NzcwMDg3NzN9.Pv1OIow11dye_uv69wnVleR93fa4fDrmup1oTXVuUuo'}
+      }).then(response =>
+          this.setState({
+            accessId: accessId,
+            matchIdList: response.data
+          })
+      )
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 매치 상세 기록 조회
+  _getMatchIdDetail = () => {
+    let MatchIdList = this.state.matchIdList;
+    let matchResultArray = [];
+
+    for (let i = 0; i < MatchIdList.length; i++) {
+      if(MatchIdList.length === 0){
+        return;
+      }else{
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        let getMatchIdDetail = 'https://api.nexon.co.kr/fifaonline4/v1.0/matches/' + MatchIdList[i];
+        try{
+          axios.get(proxyurl + getMatchIdDetail, {
+            // 헤더 값 : 권한 시리얼 정보
+            headers: {
+              Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50X2lkIjoiMTIyNDc2MTUyOSIsImF1dGhfaWQiOiIyIiwidG9rZW5fdHlwZSI6IkFjY2Vzc1Rva2VuIiwic2VydmljZV9pZCI6IjQzMDAxMTQ4MSIsIlgtQXBwLVJhdGUtTGltaXQiOiIyMDAwMDoxMCIsIm5iZiI6MTU3NzAwODc3MywiZXhwIjoxNjQwMDgwNzczLCJpYXQiOjE1NzcwMDg3NzN9.Pv1OIow11dye_uv69wnVleR93fa4fDrmup1oTXVuUuo'
+            }
+          }).then(response => {
+            matchResultArray.push(response.data);
+            this.setState({
+              matchResult: matchResultArray,
+              isLoading: false
+            })
+          })
+        }catch(error){
+          console.error(error);
+        }
+      }
+    }
   };
 
   render() {
-    const search = this.props;
-
-    const {accessId, isLoading} = this.props;
+    const { matchIdList, matchResult, isLoading} = this.state;
 
     if(!isLoading){
-      console.log("test:", accessId)
+      // matchDate 내림차순 정렬
+      function date_ascending(a, b){
+        let dateA = new Date(a['matchDate']);
+        let dateB = new Date(b['matchDate']);
+        return dateA < dateB ? 1 : -1;
+      }
+      this.state.matchResult.sort(date_ascending);
     }
 
     return (
@@ -43,7 +119,7 @@ class Header extends React.Component {
                             총 경기수
                           </CardTitle>
                           <span className="h2 font-weight-bold mb-0">
-                            22
+                            100
                           </span>
                         </div>
                         <Col className="col-auto">
